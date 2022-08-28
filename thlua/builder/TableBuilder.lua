0b62a4e8545edcecfdbc7ec82b983f7096c6c33e
cz <598799959@qq.com>
2022-08-28 23:20:07 +0800

    put ctor in Reference

--- thlua/builder/TableBuilder.lua
+++ thlua/builder/TableBuilder.lua
@@ -3,15 +3,27 @@ local UnionTerm = require "thlua.term.UnionTerm"
 local TableBuilder = {}
 
 function TableBuilder.Begin(vContext, vNode, vPairMaker)
+	local nManager = vContext._manager
+	local nTableType = nManager:LuaTable()
+	nTableType:setName("("..vContext:getPath().."-"..tostring(vNode)..")")
 	local nData = {
 		context=vContext,
 		node=vNode,
 		pairMaker=vPairMaker,
-		newTypeRefer=false,
+		luaTable=nTableType,
 	}
+	-- 1. create table
 	local nHintMethod = {
 		Self=function(self)
-			nData.newTypeRefer = vContext:getNewTypeRefer()
+			local nTagFn = vContext:getNewTagFn()
+			local nImpl = nTagFn.newTypeImpl
+			if nImpl then
+				nTableType.implType = nImpl
+			end
+			local nRefer = nTagFn.newTypeRefer
+			nRefer:setTypeAsync(function()
+				return nTableType
+			end)
 			return self
 		end,
 	}
@@ -20,26 +32,17 @@ end
 
 function TableBuilder.End(vData)
 	local nContext = vData.context
-	local nRefer = vData.newTypeRefer
 	local nManager = nContext._manager
-	-- 1. create table
-	local nTableType
-	-- 2. refer table
-	if not nRefer then
-		nTableType = nManager:LuaTable()
-		nTableType:setName("("..nContext:getPath().."-"..tostring(vData.node)..")")
-	else
-		nTableType = nRefer:checkType()
-	end
+	local nTableType = vData.luaTable
 	-- 3. set table's key value
 	local vList, vDotsStart, vDotsTuple = vData.pairMaker()
-    local nTypePairList = {}
+	local nTypePairList = {}
 	for i, nPair in ipairs(vList) do
-        local nKey = nPair[1]:getType()
+		local nKey = nPair[1]:getType()
 		local nValue = nPair[2]:getType()
-        if not nKey:isSingleton() then
-            nValue = nManager:Union(nValue, nManager.type.Nil)
-        end
+		if not nKey:isSingleton() then
+			nValue = nManager:Union(nValue, nManager.type.Nil)
+		end
 		nTypePairList[i] = {nKey, nValue}
 	end
 	if vDotsTuple then
@@ -57,7 +60,7 @@ function TableBuilder.End(vData)
 			end
 		end
 	end
-    local nKeyUnion, nTypeDict = nManager:mergePairList(nTypePairList)
+	local nKeyUnion, nTypeDict = nManager:mergePairList(nTypePairList)
 	nTableType:initByKeyValue(nKeyUnion, nTypeDict)
 	return nTableType
 end
