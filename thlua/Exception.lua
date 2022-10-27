4c6865f1fdc9c3e6d12a8614ca8caea58b9d0188
cz <598799959@qq.com>
2022-10-27 16:12:26 +0800

    add checkedUnion

--- thlua/Exception.lua
+++ thlua/Exception.lua
@@ -15,6 +15,12 @@ function Exception.new(vMsg, vNode)
 	}, Exception)
 end
 
+function Exception:fixNode(vNode)
+	if not self.node then
+		self.node = vNode
+	end
+end
+
 function Exception.is(v)
 	return getmetatable(v) == Exception
 end
