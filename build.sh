b8866a6f4785c592cc036093d954e361ed485f36
cz <598799959@qq.com>
2023-04-06 21:20:28 +0800

    add compile option for emcc build

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ rm -rf build
 mkdir build
 cd build
 # asm build:
-cmake ../ -DWASM=true -DCMAKE_CXX_FLAGS=" -s TOTAL_MEMORY=1024mb " -DCMAKE_C_FLAGS=" -s TOTAL_MEMORY=1024mb "
+FLAG=" -s SUPPORT_LONGJMP=emscripten -s TOTAL_MEMORY=512mb -s INITIAL_MEMORY=512mb -s TOTAL_STACK=512mb -sALLOW_MEMORY_GROWTH -s STACK_SIZE=5mb -s EXIT_RUNTIME=0 -s INVOKE_RUN=0 "
+cmake ../ -DWASM=1 -DCMAKE_CXX_FLAGS="$FLAG" -DCMAKE_C_FLAGS="$FLAG"
 # linux build:
 #cmake ../
 cd ..
