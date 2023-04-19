0c2399f47520492ae51fdd8e154713e19ee49608
cz <598799959@qq.com>
2023-04-19 19:40:25 +0800

    add setPath for thluaSearch

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd build
 #FLAG=" -s SUPPORT_LONGJMP=emscripten -s TOTAL_MEMORY=512mb -s INITIAL_MEMORY=64mb -s TOTAL_STACK=512mb -sALLOW_MEMORY_GROWTH -s STACK_SIZE=5mb -s EXIT_RUNTIME=0 -s INVOKE_RUN=0 "
 #cmake ../ -DWASM=1 -DCMAKE_CXX_FLAGS="$FLAG" -DCMAKE_C_FLAGS="$FLAG"
 #linux build:
-cmake ../
+cmake -DCMAKE_BUILD_TYPE=Release ../
 cd ..
-cmake --build build
+cmake --build build --config Release
