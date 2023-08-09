43f3ccc7812beb6d02dc952821093a32cfd6a7bf
cz <598799959@qq.com>
2023-08-09 14:32:41 +0800

    add script for mac x86_64

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 rm -rf build
 mkdir build
 cd build
+
 # asm build:
 #FLAG=" -s SUPPORT_LONGJMP=emscripten -s TOTAL_MEMORY=512mb -s INITIAL_MEMORY=64mb -s TOTAL_STACK=512mb -sALLOW_MEMORY_GROWTH -s STACK_SIZE=5mb -s EXIT_RUNTIME=0 -s INVOKE_RUN=0 "
 #cmake ../ -DWASM=1 -DCMAKE_CXX_FLAGS="$FLAG" -DCMAKE_C_FLAGS="$FLAG"
+
 #linux build:
 cmake -DCMAKE_BUILD_TYPE=Release ../
+
+#mac x86_64 build
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=x86_64 ../
+
 cd ..
 cmake --build build --config Release
